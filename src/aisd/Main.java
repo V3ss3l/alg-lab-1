b81d98db1f18a408166c6360ac50c9ce33d04859
V3ss3l <grishav3ss3l@gmail.com>
2022-02-07 11:57:17 +0400

    5-times run

--- src/aisd/Main.java
+++ src/aisd/Main.java
@@ -1,33 +1,44 @@
 package aisd;
+
 import java.util.Random;
+
 public class Main {
     public static long timeOfWork;
     public static int[] arr;
     public static Random rand;
 
     public static void main(String[] args) {
-        arr = new int[5000];
-        rand = new Random();
-        long begin = System.nanoTime();
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = rand.nextInt(100);
+        var n = 5000;
+        for (int i = 0; i < 5; i++) {
+            mainWork(n);
+            System.out.println('\n');
+            n+=1000;
         }
-        System.out.println(getMidAriphOfArr(arr));
-        System.out.println(getTimeOfWork(begin));
     }
 
-    public static long getTimeOfWork(long begin){
-        long finish = System.nanoTime();
+    public static long getTimeOfWork(long begin) {
+        var finish = System.nanoTime();
         timeOfWork = finish - begin;
         return timeOfWork;
     }
 
-    public static double getMidAriphOfArr(int[] arr){
-        int sum = 0;
-        int midAriph = 0;
-        for (int i : arr){
+    public static double getMidAriphOfArr(int[] arr) {
+        var sum = 0;
+        var midAriph = 0;
+        for (var i : arr) {
             sum += i;
         }
         return midAriph = sum / arr.length;
+    }
+
+    public static void mainWork(int n) {
+        arr = new int[n];
+        rand = new Random();
+        long begin = System.nanoTime();
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        System.out.println(getMidAriphOfArr(arr));
+        System.out.println(getTimeOfWork(begin));
     }
 }
